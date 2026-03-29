7cd20a824b765ea7e07cdd0b47e97a97283417cc
Gaurav Kumar Borad <gjain7764@gmail.com>
2026-03-29 14:09:54 +0530

    this is my spring basic learning

---
+++ FirstProject/src/main/java/com/gaurav/firstproject/Laptop.java
@@ -0,0 +1,10 @@
+package com.gaurav.firstproject;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+    public void compile() {
+        System.out.println("complie it");
+    }
+}
