d684f86a7f24771aca489d3a12cfc0a37866f880
Gaurav Kumar Borad <gjain7764@gmail.com>
2026-04-02 08:17:31 +0530

    this is basic product project

---
+++ ProductDemo/src/main/java/com/gaurav/productdemo/Product.java
@@ -0,0 +1,15 @@
+package com.gaurav.productdemo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    private String name;
+    private String type;
+    private String place;
+    private int warranty;
+}
